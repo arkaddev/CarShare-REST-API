85ac48ebc5d0442bf54e828c7eb530676327b1b6
arkaddev <arek1994mr@gmail.com>
2025-04-13 20:16:37 +0200

    CarShare project was created

---
+++ src/main/java/com/example/CarShare/CarShareApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CarShare/CarShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
