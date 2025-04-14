72f8c508d9edaf988582cfec4485b3b9681e5a1a
arkaddev <arek1994mr@gmail.com>
2025-04-14 02:12:38 +0200

    UserController.java was created

---
+++ src/main/java/com/example/CarShare/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.CarShare.repository;
+
+import com.example.CarShare.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
